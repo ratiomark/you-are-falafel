ac29f7dbc6520597a77d7229a7c33dc0526d767f
ratiomark <yanagae@gmail.com>
2025-07-23 14:36:23 +0300

    добавил пиксель

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import Head from 'next/head';
 import backgroundImage from 'public/background.png';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
+import MetaPixel from '@/app/MetaPixel';
 
 const PostHogPageView = dynamic(() => import('./PostHogPageView'), {
   ssr: false,
@@ -78,7 +79,23 @@ export function generateMetadata() {
   };
   return metadata;
 }
-
+{
+  /* <script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1231745995361917');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1231745995361917&ev=PageView&noscript=1"
+/></noscript> */
+}
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -87,6 +104,7 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
+        <head></head>
         {/* <Head> */}
         {/* <link
             rel="apple-touch-icon"
@@ -139,7 +157,7 @@ export default function RootLayout({
             content="#ffffff"
           /> */}
         {/* </Head> */}
-        <PHProvider>
+        {/* <PHProvider> */}
           <body
             // className={inter.className}
             // className={cn('min-h-screen bg-background font-sans antialiased', inter.className)}
@@ -147,21 +165,13 @@ export default function RootLayout({
             // className={cn('min-h-screen bg-background font-sans antialiased', inter.variable, inter.className)}
             suppressHydrationWarning
           >
-            <PostHogPageView />
+            {/* Meta / Facebook Pixel */}
+            <MetaPixel />
+            {/* <PostHogPageView /> */}
             {children}
-            {/* <Image
-            src={backgroundImage}
-            alt=""
-            placeholder="blur"
-            fill
-            style={{
-              objectFit: 'cover',
-              zIndex: -1,
-            }}
-          /> */}
             <Toaster />
           </body>
-        </PHProvider>
+        {/* </PHProvider> */}
       </html>
     </>
   );
