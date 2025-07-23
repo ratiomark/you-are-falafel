4d5126dfc5b41c6fac17cf86501d31594af30097
ratiomark <yanagae@gmail.com>
2025-07-23 14:46:51 +0300

    добавил пиксель

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ import backgroundImage from 'public/background.png';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import MetaPixel from '@/app/MetaPixel';
+import Script from 'next/script';
 
 const PostHogPageView = dynamic(() => import('./PostHogPageView'), {
   ssr: false,
@@ -104,6 +105,12 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
+        <Head>
+          <link
+            rel="canonical"
+            href="https://www.webdevelopment.lk"
+          />
+        </Head>
         <head></head>
         {/* <Head> */}
         {/* <link
@@ -158,19 +165,47 @@ export default function RootLayout({
           /> */}
         {/* </Head> */}
         {/* <PHProvider> */}
-          <body
-            // className={inter.className}
-            // className={cn('min-h-screen bg-background font-sans antialiased', inter.className)}
-            className={cn('custom-scrollbar-2 min-h-screen bg-background font-sans antialiased', fontSans.variable)}
-            // className={cn('min-h-screen bg-background font-sans antialiased', inter.variable, inter.className)}
-            suppressHydrationWarning
-          >
-            {/* Meta / Facebook Pixel */}
-            <MetaPixel />
-            {/* <PostHogPageView /> */}
-            {children}
-            <Toaster />
-          </body>
+        <body
+          // className={inter.className}
+          // className={cn('min-h-screen bg-background font-sans antialiased', inter.className)}
+          className={cn('custom-scrollbar-2 min-h-screen bg-background font-sans antialiased', fontSans.variable)}
+          // className={cn('min-h-screen bg-background font-sans antialiased', inter.variable, inter.className)}
+          suppressHydrationWarning
+        >
+          {/* Meta / Facebook Pixel */}
+          <MetaPixel />
+          {/* <PostHogPageView /> */}
+          {children}
+          <Toaster />
+          <>
+            <Script
+              id="facebook-pixel"
+              strategy="afterInteractive"
+            >
+              {`
+         !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=true;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1231745995361917');
+            fbq('track', 'PageView');
+        `}
+            </Script>
+
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: 'none' }}
+                src="https://www.facebook.com/tr?id=1231745995361917&ev=PageView&noscript=1"
+              />
+            </noscript>
+          </>
+        </body>
         {/* </PHProvider> */}
       </html>
     </>
